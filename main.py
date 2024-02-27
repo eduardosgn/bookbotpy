d0859e9134347124e6424546e61acd15e792e2f4
Eduardo Nascimento <eduardosgn19@gmail.com>
2024-02-27 12:51:23 -0300

    project complete

---
+++ main.py
@@ -0,0 +1,42 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_letters(text):
+    counts = {}
+    for char in text.lower():
+        if char.isalpha():
+            if char in counts:
+                counts[char] += 1
+            else:
+                counts[char] = 1
+    return counts
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+    
+def sort_counts(counts):
+    counts_list = []
+    for letter, count in counts.items():
+        item_dict = {"letter": letter, "count": count}
+        counts_list.append(item_dict)
+    counts_list.sort(key=lambda x: x["count"], reverse=True)
+    return counts_list
+    
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    letter_count = count_letters(text)
+    sorted_letter_counts = sort_counts(letter_count)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+
+    for letter_count in sorted_letter_counts:
+        print(f"The '{letter_count['letter']}' character was found {letter_count['count']} times")
+
+    print("--- End report ---")
+
+main()
